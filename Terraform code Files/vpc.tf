b34375bbfb8a3f794bef57f3eb1ce0a7efcd2cbd
Bilal Ahmad <120258415+engrbilal1@users.noreply.github.com>
2024-02-20 18:16:58 +0500

    Update vpc.tf

--- Terraform code Files/vpc.tf
+++ Terraform code Files/vpc.tf
@@ -6,8 +6,7 @@ module "vpc" {
   azs            = ["us-east-1a", "us-east-1b"]
   public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   tags = {
-    "env"       = "dev"
-    "createdBy" = "binpipe"
+    "env"       = "test"
   }
 
 }
